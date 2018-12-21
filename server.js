689792ee491c41fe4e987db1c7370aff38ed389a
scorpion9979 <ahmed_ihsan_tawfeek@yahoo.com>
2018-12-21 12:49:18 +0300

    Revamped email template

--- server.js
+++ server.js
@@ -21,11 +21,83 @@ app.post("/send-email", function(req, res) {
         }
     });
     let mailOptions = {
-        from: '"' + req.body.name + '" <' + req.body.email + '>',
+        from: `"${req.body.name}" <${req.body.email}>`,
         to: process.env.RECEPIENT_EMAIL,
-        subject: "New mail for Ahmed the Ninja",
-        // TODO: add phone info
-        html: "<b>Received via Ahmed the Ninja</b><p>" + req.body.message + "</p>"
+        subject: `New mail for Ahmed Tawfeeq`,
+        // email template from:
+        // https://zurb.com/playground/projects/responsive-email-templates/basic.html
+        html: 
+        `
+        <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+        <html xmlns="http://www.w3.org/1999/xhtml">
+        <head>
+            <!-- If you delete this meta tag, Half Life 3 will never be released. -->
+            <meta name="viewport" content="width=device-width">
+            <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+            <title>ZURBemails</title>
+            <style type="text/css">
+            @media only screen and (max-width: 600px) {
+                a[class="btn"] { display:block!important; margin-bottom:10px!important; background-image:none!important; margin-right:0!important;}
+                div[class="column"] { width: auto!important; float:none!important;}
+                table.social div[class="column"] {
+                    width:auto!important;
+                }
+            }
+            </style>
+        </head>
+        <body bgcolor="#FFFFFF" style='margin:0;padding:0;font-family:"Helvetica Neue", "Helvetica", Helvetica, Arial, sans-serif;-webkit-font-smoothing:antialiased;-webkit-text-size-adjust:none;height:100%;width:100% !important;'>
+            <!-- BODY -->
+            <table class="body-wrap" style='width:100%;'>
+            <tr>
+                <td></td>
+                <td class="container" bgcolor="#FFFFFF" style='display:block !important;max-width:600px !important;margin:0 auto !important;clear:both !important;'>
+                <div class="content" style='padding:15px;max-width:600px;margin:0 auto;display:block;'>
+                    <table style='width:100%;'>
+                    <tr>
+                        <td>
+                        <p class="lead" style='white-space: pre;margin-bottom:10px;font-weight:normal;font-size:14px;line-height:1.6;font-size:17px;'>${req.body.message}</p>
+                        <br/>
+                        <!-- social & contact -->
+                        <table class="social" width="100%" style='background-color:#ebebeb;width:100%;'>
+                            <tr>
+                            <td>
+                                <!-- column 2 -->
+                                <table align="left" class="column" style='width:300px;width:100%;width:280px;min-width:279px;float:left;'>
+                                <tr>
+                                    <td style='padding:15px;'>
+                                    <h5 class="" style='font-family:"HelveticaNeue-Light", "Helvetica Neue Light", "Helvetica Neue", Helvetica, Arial, "Lucida Grande", sans-serif;line-height:1.1;margin-bottom:15px;color:#000;font-weight:900;font-size:17px;'>Contact Info:</h5>
+                                    <p style='margin-bottom:10px;font-weight:normal;font-size:14px;line-height:1.6;'>
+                                    Phone: <strong>
+                                        ${req.body.phone}
+                                    </strong>
+                                    <br/>
+                                    Email: <strong>
+                                        <a href="emailto:hseldon@trantor.com" style='color:#2BA6CB;'>
+                                            ${req.body.email}
+                                        </a>
+                                    </strong></p>
+                                    </td>
+                                </tr>
+                                </table>
+                                <!-- /column 2 -->
+                                <span class="clear" style='display:block;clear:both;'></span>
+                            </td>
+                            </tr>
+                        </table>
+                        <!-- /social & contact -->
+                        </td>
+                    </tr>
+                    </table>
+                </div>
+                <!-- /content -->
+                </td>
+                <td></td>
+            </tr>
+            </table>
+            <!-- /BODY -->
+        </body>
+        </html>
+        `
     };
     transporter.sendMail(mailOptions, (error, info) => {
         if (error) {
